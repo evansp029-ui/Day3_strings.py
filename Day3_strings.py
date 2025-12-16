bc25a365f1a3af072ab833da3b7301ab209fd979
evansp029-ui <evansp029@gmail.com>
2025-12-16 17:16:31 +0100

    Add Day 3 string practice

---
+++ Day3_strings.py
@@ -0,0 +1,7 @@
+name = "Peter"
+Skill = "Generative AI"
+Country = "Nigeria"
+print ("Hello, my name is", name)
+print ("I am learning skill", Skill)
+print ("I am based in", Country)
+print ("I am building my tech career using my phone")
